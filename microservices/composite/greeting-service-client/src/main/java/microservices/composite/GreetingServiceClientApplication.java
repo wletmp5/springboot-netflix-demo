74f5b7642ed5cba133dfb513b799aba3f60ea987
glenndennismorales <wletmp5@yahoo.com>
2018-02-04 01:06:10 -0500

    added zipkin service and server

--- microservices/composite/greeting-service-client/src/main/java/microservices/composite/GreetingServiceClientApplication.java
+++ microservices/composite/greeting-service-client/src/main/java/microservices/composite/GreetingServiceClientApplication.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableFeignClients
 @EnableHystrix
 @EnableZuulProxy
+@EnableEurekaClient
 public class GreetingServiceClientApplication {
 
     GreetingServiceClient greetingServiceClient;
--- microservices/core/greeting-service/src/main/java/microservice/core/GreetingServiceApplication.java
+++ microservices/core/greeting-service/src/main/java/microservice/core/GreetingServiceApplication.java
@@ -2,6 +2,7 @@ package microservice.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableZuulProxy
+@EnableEurekaClient
 public class GreetingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GreetingServiceApplication.class, args);
